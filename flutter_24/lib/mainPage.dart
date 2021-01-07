338e63baf70cb2651a09cb3900866dac7a9302ea
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-08 00:11:29 +0800

    try

---
+++ flutter_24/lib/mainPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
