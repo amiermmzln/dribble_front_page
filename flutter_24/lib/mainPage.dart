179cd92ebfdb40d0816064a03f0370cd613710a0
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-08 10:04:12 +0800

    updated

--- flutter_24/lib/mainPage.dart
+++ flutter_24/lib/mainPage.dart
@@ -3,8 +3,53 @@ import 'package:flutter/material.dart';
 class MainPage extends StatelessWidget {
   double getSmallDiameter(BuildContext context) =>
       MediaQuery.of(context).size.width * 2 / 3;
+  double getBigDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            left: -getBigDiameter(context) / 4,
+            top: -getBigDiameter(context) / 4,
+            child: Container(
+              child: Center(
+                child: Text(
+                  'dribble',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 30,
+                      color: Colors.white),
+                ),
+              ),
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
