1e5383e6a131ea96aff835a03f859cd6d1ed0063
AMIER MUSTHAKEEN <73897840+amiermmzln@users.noreply.github.com>
2021-01-08 11:50:46 +0800

    tmbah kotak

--- flutter_24/lib/mainPage.dart
+++ flutter_24/lib/mainPage.dart
@@ -9,6 +9,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEEEEEE),
       body: Stack(
         children: <Widget>[
           Positioned(
@@ -46,6 +47,31 @@ class MainPage extends StatelessWidget {
                       colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter)),
+            ),
+          ),
+          Positioned(
+            right: -getBigDiameter(context) / 2,
+            bottom: -getBigDiameter(context) / 2,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Color(0xFFF3E9EE)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5)),
+                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
+                )
+              ],
             ),
           )
         ],
